4f0e8e6a290a445f9a1bb6bb555dec9c1960e23a
ArielBenichou <videojr3000@users.noreply.github.com>
2023-04-09 19:23:22 +0300

    call power bar when painting

--- Assets/Scripts/TilePainter.cs
+++ Assets/Scripts/TilePainter.cs
@@ -5,8 +5,17 @@ using UnityEngine.Tilemaps;
 
 public class TilePainter : MonoBehaviour
 {
+    public string playerColor;
     public Tilemap floorTilemap;
     public Color colorToPaint = Color.magenta;
+    public PowerBar powerBar;
+
+    private static Dictionary<Vector2, string> tileTracker;
+
+    void Start()
+    {
+        tileTracker = new(){};
+    }
 
     void FixedUpdate()
     {
@@ -14,12 +23,28 @@ public class TilePainter : MonoBehaviour
         myPos.y = Mathf.Floor(myPos.y);
         myPos.x = Mathf.Floor(myPos.x);
         this.ChangeTileColor(Vector2Int.RoundToInt(myPos), colorToPaint);
+
+        if(!IsPlayerAlreadyPaintedTile(myPos))
+        {
+            powerBar.FillPowerBar();
+        }
+        tileTracker[myPos] = playerColor;
     }
 
-    void ChangeTileColor(Vector2Int tilePos, Color color)
+    private void ChangeTileColor(Vector2Int tilePos, Color color)
     {
         Vector3Int myPos = (Vector3Int)tilePos;
         floorTilemap.SetTileFlags(myPos, TileFlags.None);
         floorTilemap.SetColor(myPos, color);
+    }
+
+    private bool IsPlayerAlreadyPaintedTile(Vector2 pos)
+    {
+        if(tileTracker.ContainsKey(pos))
+        {
+            return tileTracker[pos] == playerColor;
+        }
+
+        return false;
     }
 }
